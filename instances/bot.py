13dad33f3aa6a193c82f1175cb73105580d5bab2
kaeeraa <kaeeraa@nebula-nook.ru>
2024-10-16 00:07:03 +0300

    instances: add bot

--- instances/bot.py
+++ instances/bot.py
@@ -1,9 +1,7 @@
-from hikari import (
-    GatewayBot
-)
+from hikari import GatewayBot
 from dotenv import load_dotenv
 from os import environ
 
 load_dotenv()
 
-bot = GatewayBot(token=environ['BOT_TOKEN'])
+bot = GatewayBot(token=environ["BOT_TOKEN"])
