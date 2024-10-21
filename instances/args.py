67e330f14c5a8f59c0eae9c4b042e82b55a9e32e
kaeeraa <kaeeraa@nebula-nook.ru>
2024-10-21 18:59:17 +0300

    instances: create arg parser

---
+++ instances/args.py
@@ -0,0 +1,16 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser(
+    prog="Matter bot",
+    description="An all-in-one ticket bot for Discord.",
+    epilog="https://github.com/kaeeraa/Matter",
+)
+
+parser.add_argument(
+    "-V", "--verbose", action="store_true", help="Enable verbose logging", default=False
+)
+parser.add_argument(
+    "-VV", "--vomit", action="store_true", help="Enable trace (vomit) logging", default=False
+)
+
+args = parser.parse_args()
