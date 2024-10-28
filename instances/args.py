4a442491fe10976d2a3be69118d14f70faa2bc4b
kaeeraa <kaeeraa@nebula-nook.ru>
2024-10-28 12:16:14 +0300

    args: fix verbose and vomit flags

--- instances/args.py
+++ instances/args.py
@@ -8,7 +8,7 @@ parser = ArgumentParser(
     epilog="https://github.com/kaeeraa/Matter",
 )
 
-parser.add_argument("-V", "--verbose", action="store_true", help="Enable verbose logging", default=False)
-parser.add_argument("-VV", "--vomit", action="store_true", help="Enable trace (vomit) logging", default=False)
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose logging", default=False)
+parser.add_argument("-vv", "--vomit", action="store_true", help="Enable trace (vomit) logging", default=False)
 
 args = parser.parse_args()
