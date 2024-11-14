f1229e78dc653d84f6e16109b594ae4b4b4ea490
kaeeraa <kaeeraa@nebula-nook.ru>
2024-11-14 21:22:01 +0300

    helpers: update embed (rewrite docstring)

--- helpers/embed.py
+++ helpers/embed.py
@@ -9,24 +9,26 @@ from hikari.embeds import Embed
 
 def new_embed(ctx: GatewayContext, title: str, description: str) -> Embed:
     """
-    Creates and returns a new Embed object.
+    Generate a new Embed object with specified title and description.
 
     Args:
-        ctx (GatewayContext): The context that invoked this command, used for author details.
-        title (str): The title of the embed.
-        description (str): The description of the embed.
+        ctx (GatewayContext): Context of the command invocation for author details.
+        title (str): Title of the embed.
+        description (str): Description content of the embed.
 
     Returns:
-        Embed: A newly created Embed object with the specified title, description, and a footer
-        indicating the author and current time.
+        Embed: An Embed object initialized with the provided title, description,
+        and a footer showing the author's name and the current time.
     """
     embed: Embed = Embed(
         title=title,
         description=description,
         color=Color.from_rgb(90, 0, 240),
     )
+
     embed.set_footer(
         text=f"Matter  •  Called by @{ctx.author.username}  •  {datetime.now().strftime('%H:%M:%S')}",
         icon=ctx.author.avatar_url,
     )
+
     return embed
