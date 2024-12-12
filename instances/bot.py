62d594755dbfd1dcca84621124a8b5a6f9586fe5
kaeeraa <kaeeraa@nebula-nook.ru>
2024-12-12 19:23:58 +0300

    chore(instances.bot): make paths absolute

--- instances/bot.py
+++ instances/bot.py
@@ -6,9 +6,11 @@ from arc import GatewayClient
 from dotenv import load_dotenv
 from hikari import GatewayBot, Intents
 
+from root import rootDir
+
 from instances.log import logger
 
-load_dotenv()
+load_dotenv(f"{rootDir}/configuration/.env")
 
 logger.trace("Initializing bot")
 bot = GatewayBot(token=environ["BOT_TOKEN"], intents=Intents.ALL)
