71e63616651555068da7b43763b8eac23a4a3feb
kaeeraa <kaeeraa@nebula-nook.ru>
2024-12-13 16:57:57 +0300

    feat(instances.bot): completely remove .env support

--- instances/bot.py
+++ instances/bot.py
@@ -1,18 +1,13 @@
 """This module contains the bot and client initialization logic."""
 
-from os import environ
-
 from arc import GatewayClient
-from dotenv import load_dotenv
 from hikari import GatewayBot, Intents
 
+from instances.config import Config
 from instances.log import logger
-from root import rootDir
-
-load_dotenv(f"{rootDir}/configuration/.env")
 
 logger.trace("Initializing bot")
-bot = GatewayBot(token=environ["BOT_TOKEN"], intents=Intents.ALL)
+bot = GatewayBot(token=Config().getToken(), intents=Intents.ALL)
 
 logger.trace("Initializing client")
 client = GatewayClient(app=bot)
