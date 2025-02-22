2da708f79a9b5506b4318611725f041006fc9525
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-22 23:06:35 +0300

    feat(i18n): rewrite i18n
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/matter/core/classes/i18n.py
+++ src/matter/core/classes/i18n.py
@@ -5,52 +5,21 @@ from typing import Any
 from pyjson5 import loads
 
 from matter import ROOT
+from matter.core.classes.config import Config
 
 
 class Locale:
-    """A Locale i18n class
-
-    Returns:
-        str: as described in methods
-    """
-
-    _locale: str = "en_US"
-    _data: dict[str, Any]
+    _locale: str = Config().getDictionary("general")["language"]
+    data: dict[str, Any] = {}
     _path: str = f"{ROOT}/configuration/lang/{_locale}.json5"
 
     def __init__(self):
-        """Init Locale class with _locale"""
-
         with open(self._path, "r", encoding="utf-8") as f:
-            self._data = loads(f.read())
+            self.data = loads(f.read())
 
-    def setLocale(self, locale: str):
-        """Change locale file
 
-        Args:
-            locale (str): locale file name
-        """
-        self._locale = locale
-        self._path = f"/configuration/lang/{locale}.json5"
+_locale = Locale()
 
-        with open(f"/configuration/lang/{locale}.json5", "r", encoding="utf-8") as f:
-            self._data = loads(f.read())
 
-    def current(self) -> str:
-        """Get current locale
-
-        Returns:
-            str: name of current locale
-        """
-        return self._locale
-
-    def path(self) -> str:
-        """Get path to current locale
-
-        Returns:
-            str: path to locale
-        """
-        return f"/configuration/lang/{self._locale}.json5"
-
-    def tr(self, text: str) -> str:
-        return self._data.get(text, text)
+def tr(text: str) -> str:
+    return _locale.data.get(text, text)
