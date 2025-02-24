61cbf11a4aec663feeb2c01de01215ba74679f8e
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-24 23:09:13 +0300

    chore(core): add hints, add docstrings
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/matter/core/build/BuildConfig.py
+++ src/matter/core/build/BuildConfig.py
@@ -1,3 +1,11 @@
+"""Generate build constants from project configuration
+
+Raises:
+    ProjectConfigurationNotFound: Raised if no pyproject.toml is found
+
+Returns:
+    str: Return generated constant
+"""
 from typing import Any
 from tomli import loads
 from matter.core.classes.exceptions import ProjectConfigurationNotFound
@@ -5,18 +13,25 @@ from matter.core.classes.logger import logger
 from matter import PROJECT_ROOT
 
 
-class BuildConfig:
+class BuildConfig(object):
+    """
+    Raises:
+        ProjectConfigurationNotFound: Raised if no pyproject.toml is found
+
+    Returns:
+        str: Return generated constant
+    """
     _config: dict[str, Any] = {}
     _content: str = ""
 
     try:
-        with open(f"{PROJECT_ROOT}/pyproject.toml", "r", encoding="utf-8") as f:
+        with open(file=f"{PROJECT_ROOT}/pyproject.toml", mode="r", encoding="utf-8") as f:
             _content = f.read()
         _config = loads(_content).get("tool", {}).get("poetry", {})
     except FileNotFoundError:
         raise ProjectConfigurationNotFound
 
-    def __init_subclass__(cls) -> None:
+    def __init_subclass__(cls: type[Any]) -> None:
         for key, value in cls._config.items():
             setattr(cls, key.upper(), value)
 
