cf8fff4235c32865108473d09097da3f4fba5a6a
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-24 22:22:54 +0300

    chore(helpers): add hints, create docstring
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/matter/core/helpers/embed.py
+++ src/matter/core/helpers/embed.py
@@ -8,11 +8,27 @@ from hikari.embeds import Embed
 
 from matter.core.classes.i18n import tr
 
-EMBED_COLOR = Color.from_rgb(90, 0, 240)
+EMBED_COLOR: Color = Color.from_rgb(90, 0, 240)
 TIME_FORMAT = "%H:%M:%S"
 
 
-def embed(author: User, title: str, description: str, color: Optional[Color] = EMBED_COLOR) -> Embed:
+def embed(
+    author: User,
+    title: str,
+    description: str,
+    color: Optional[Color] = EMBED_COLOR
+) -> Embed:
+    """Generate embed by template
+
+    Args:
+        author (User): User object, referenced as author
+        title (str): Title of embed
+        description (str): Description of embed
+        color (Optional[Color], optional): Color of embed. Defaults to EMBED_COLOR.
+
+    Returns:
+        Embed: Filled embed
+    """
 
     _embed: Embed = Embed(
         title=title,
@@ -22,7 +38,7 @@ def embed(author: User, title: str, description: str, color: Optional[Color] = E
 
     _footerTemplate: str = tr("Matter  •  Called by @{author}  •  at {date}")
 
-    _footerText = _footerTemplate.format(author=author.username, date=datetime.now().strftime(TIME_FORMAT))
+    _footerText: str = _footerTemplate.format(author=author.username, date=datetime.now().strftime(TIME_FORMAT))
 
     _embed.set_footer(
         text=_footerText,
