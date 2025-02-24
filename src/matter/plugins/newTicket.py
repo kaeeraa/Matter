509269e10afcd1cfcd6a2f3b7cf6634f9e43308d
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-24 22:17:05 +0300

    chore(newTicket): add hints
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/matter/plugins/newTicket.py
+++ src/matter/plugins/newTicket.py
@@ -20,12 +20,14 @@ from matter.core.classes.ticket import Ticket
 from matter.core.classes.tickets.ticketManager import TicketManager
 
 COMMAND_NAME = "new"
-COMMAND_DESCRIPTION = tr("Create a new ticket")
+COMMAND_DESCRIPTION: str = tr("Create a new ticket")
 
 
 @slash_command(name="new", description=COMMAND_DESCRIPTION)
 async def newTicket(
-    ctx: GatewayContext, title: Option[str, str] = "Ndfined", description: Option[str, str] = "None"
+    ctx: GatewayContext,
+    title: Option[str, str] = "Ndfined",
+    description: Option[str, str] = "None"
 ) -> None:
     """
     Creates a new ticket and sends a message to the channel.
@@ -41,7 +43,7 @@ async def newTicket(
     logger.trace(tr("/{command} command called ({author})").format(command=COMMAND_NAME, author=ctx.author.username))
     # Return config fully
     _config: dict[str, Any] = Config().getDictionary("")
-    _ticket: Ticket = TicketManager().newTicket(ctx.author, title, description)
+    _ticket: Ticket = TicketManager().newTicket(author=ctx.author, title=title, description=description)
 
     if guild := ctx.get_guild():
         name: str = _config.get("brain.tickets.format", "{username}-{counter}")
