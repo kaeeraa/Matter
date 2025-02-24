635ecb432e42890c6e8da2c5784089d4b0c1270e
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-24 22:29:22 +0300

    chore(core): add hints, add docstrings
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/matter/core/classes/i18n.py
+++ src/matter/core/classes/i18n.py
@@ -9,14 +9,15 @@ from matter.core.classes.config import Config
 from matter.core.classes.logger import logger
 
 
-class Locale:
+class Locale(object):
+    """Find and operate with locales"""
     _locale: str = Config().getDictionary("general")["language"]
     data: dict[str, Any] = {}
     _path: str = f"{ROOT}/configuration/lang/{_locale}.json5"
 
-    def __init__(self):
+    def __init__(self) -> None:
         try:
-            with open(self._path, "r", encoding="utf-8") as f:
+            with open(self._path, mode="r", encoding="utf-8") as f:
                 self.data = loads(f.read())
         except FileNotFoundError:
             logger.warning("Locale {} not found. Using en_US".format(self._locale))
@@ -27,4 +28,12 @@ _locale = Locale()
 
 
 def tr(text: str) -> str:
+    """Translate text, return the same text if key is not found
+
+    Args:
+        text (str): Text to translate
+
+    Returns:
+        str: Translated text
+    """
     return _locale.data.get(text, text)
